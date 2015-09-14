bb1ffff5c88c69a0e5ff47538a45fbd6bbcc0091
Nikolay Kluyev <kluyev.nikolay@gmail.com>
2015-09-14 23:30:12 +0300

    all task condition have been met

--- main.js
+++ main.js
@@ -1,36 +1,50 @@
 var localData = [];
+var dataRows;
 $(document).ready(
     function(){
+        jQuery.ajax("https://public.opencpu.org/ocpu/library/", {
+            dataType: "text",
+            success: fillArray
+        });
         $("#load").click(
-            function(event){
-                var dataRows = Number($("#item-count").val());
+            function(){
+                dataRows = Number($("#item-count").val());
                 if (dataRows > 0){
-                    jQuery.ajax("https://public.opencpu.org/ocpu/library/", {
-                        dataType: "text",
-                        success: loadData
-                    });
+                    removeTable();
+                    drawTable();
                 }
+            }
+        );
+        $("#clear").click(
+            function(){
+                removeTable();
             }
         );
     }
 );
-
-function loadData(fromServer){
-    var dataRows = Number($("#item-count").val());
-    var resultArray = [];
-    for (var i = 0; i<dataRows; i++){
-        resultArray.push(fromServer.split("\n")[i]);
-    }
-    createTable(resultArray);
+function fillArray(data){
+    localData = data.split("\n");
 }
+function removeTable(){
+    $("#data-table").children().remove();
+}
+function drawTable(){
+    var tableHeader = $("<tr/>").appendTo('#data-table');
+    tableHeader.append($("<td>Number</td>"));
+    tableHeader.append($("<td>Item</td>"));
 
-function createTable(dataArray){
-    for (var i=0; i < dataArray.length; i++){
+    for (var i=0; i < dataRows; i++){
         var row = $("<tr/>", {class: "data-row", id: "data-row-"+(i+1)}).appendTo('#data-table');
         row.append($("<td class='number-col'>"+ (i+1) +"</td>"));
-        row.append($("<td>"+ dataArray[i] +"</td>"));
+        row.append($("<td class='item-col'>" + localData[i] + "</td>"));
     }
+
     $(".number-col").click(function(){
         $(this).parent().remove();
     });
+
+    $(".item-col").click(function(){
+        var win = window.open("https://translate.google.ru/", '_blank');
+        win.focus();
+    })
 }
